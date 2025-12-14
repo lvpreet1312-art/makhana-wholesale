eed626bab648f784a2fbde65315e7cf0fc5dee9d
lvpreet1312-art <sagar.dubey7665@gmail.com>
2025-12-14 20:50:52 +0530

    Short clear message

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
               href="/products"
               className="text-gray-700 hover:text-green-600 transition px-3 py-2 text-sm font-medium relative group"
             >
-              Products
+              Product
               <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-green-600 group-hover:w-full transition-all duration-300"></span>
             </Link>
             <Link
